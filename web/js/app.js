be9932305c231f595b4c5be4ba72af10bed1045c
Owen Smith <owen@omsmith.ca>
2013-05-04 18:54:17 -0400

    some addtional start on setup-ering

---
+++ web/js/app.js
@@ -0,0 +1,8 @@
+var app = app || {};
+
+$(function () {
+	'use strict';
+
+	// kick things off by creating the `App`
+	new app.AppView();
+});
---
+++ web/js/views/app.js
@@ -0,0 +1,11 @@
+var app = app || {};
+
+(function ($) {
+	'use strict';
+
+	// overall view
+	app.AppView = Backbone.View.extend({
+
+	});
+
+})(jQuery);
