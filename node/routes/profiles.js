964f48cac0e59517ea7e4be82fa50bc8eb4af022
Andrew Davidson <aadavids@uwaterloo.ca>
2013-05-04 21:23:08 -0400

    created REST api for the application, added mongodb support, added sample data

--- node/routes/profiles.js
+++ node/routes/profiles.js
@@ -1,7 +1,115 @@
-exports.findAll = function(req, res) {
-	res.send([{name:'profile1'}, {name:'profile2'}, {name:'profile3'}]);
-};
-
+var mongo = require('mongodb');
+ 
+var Server = mongo.Server,
+    Db = mongo.Db,
+    BSON = mongo.BSONPure;
+ 
+var server = new Server('localhost', 27017, {auto_reconnect: true});
+db = new Db('profiledb', server, {safe:false});
+ 
+db.open(function(err, db) {
+console.log('starting routes/profile');
+    if(!err) {
+        console.log("Connected to 'profiledb' database");
+        db.collection('profiles', {strict:true}, function(err, collection) {
+            if (err) {
+                console.log("The 'profiles' collection doesn't exist. Creating it with sample data...");
+                populateDB();
+            }
+        });
+    }
+    else {
+    	console.log("we have an error");
+    }
+});
+ 
 exports.findById = function(req, res) {
-	res.send({id:req.params.id, name: "The Name", description: "description"});
+    var id = req.params.id;
+    console.log('Retrieving profile: ' + id);
+    db.collection('profiles', function(err, collection) {
+        collection.findOne({'_id':new BSON.ObjectID(id)}, function(err, item) {
+            res.send(item);
+        });
+    });
+};
+ 
+exports.findAll = function(req, res) {
+    db.collection('profiles', function(err, collection) {
+        collection.find().toArray(function(err, items) {
+            res.send(items);
+        });
+    });
+};
+ 
+exports.addprofile = function(req, res) {
+    var profile = req.body;
+    console.log('Adding profile: ' + JSON.stringify(profile));
+    db.collection('profiles', function(err, collection) {
+        collection.insert(profile, {safe:true}, function(err, result) {
+            if (err) {
+                res.send({'error':'An error has occurred'});
+            } else {
+                console.log('Success: ' + JSON.stringify(result[0]));
+                res.send(result[0]);
+            }
+        });
+    });
+}
+ 
+exports.updateprofile = function(req, res) {
+    var id = req.params.id;
+    var profile = req.body;
+    console.log('Updating profile: ' + id);
+    console.log(JSON.stringify(profile));
+    db.collection('profiles', function(err, collection) {
+        collection.update({'_id':new BSON.ObjectID(id)}, profile, {safe:true}, function(err, result) {
+            if (err) {
+                console.log('Error updating profile: ' + err);
+                res.send({'error':'An error has occurred'});
+            } else {
+                console.log('' + result + ' document(s) updated');
+                res.send(profile);
+            }
+        });
+    });
+}
+ 
+exports.deleteprofile = function(req, res) {
+    var id = req.params.id;
+    console.log('Deleting profile: ' + id);
+    db.collection('profiles', function(err, collection) {
+        collection.remove({'_id':new BSON.ObjectID(id)}, {safe:true}, function(err, result) {
+            if (err) {
+                res.send({'error':'An error has occurred - ' + err});
+            } else {
+                console.log('' + result + ' document(s) deleted');
+                res.send(req.body);
+            }
+        });
+    });
+}
+ 
+/*----------------------------------------------------------------------------------------------*/
+// Populate database with sample data -- Only used once: the first time the application is started.
+// You'd typically not find this code in a real-life app, since the database would already exist.
+var populateDB = function() {
+ 
+    var profiles = [
+    {
+        name: "ANDREW DAVIDSON",
+        year: "20",
+    },
+    {
+        name: "OWEN SMITH",
+        year: "21",
+    },
+    {
+        name: "JOSH MACKENZIE",
+        year: "20",
+    }];
+ 
+    db.collection('profiles', function(err, collection) {
+        collection.insert(profiles, {safe:true}, function(err, result) {});
+    });
+ 
 };
--- node/server.js
+++ node/server.js
@@ -1,10 +1,18 @@
 var express = require('express'),
-    profiles = require('./routes/profiles');
-
+    profile = require('./routes/profiles');
+ 
 var app = express();
-
-app.get('/profiles', profiles.findAll);
-app.get('/profiles/:id', profiles.findById);
-
+ 
+app.configure(function () {
+    app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
+    app.use(express.bodyParser());
+});
+ 
+app.get('/profiles', profile.findAll);
+app.get('/profiles/:id', profile.findById);
+app.post('/profiles', profile.addprofile);
+app.put('/profiles/:id', profile.updateprofile);
+app.delete('/profiles/:id', profile.deleteprofile);
+ 
 app.listen(3000);
 console.log('Listening on port 3000...');
