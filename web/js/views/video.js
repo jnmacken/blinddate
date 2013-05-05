f7b88a16eb90d247bbe45321339fd3320edb14aa
Owen Smith <owen@omsmith.ca>
2013-05-04 23:34:23 -0400

    switch to webrtc.io for client. server coming by andrew

--- web/js/views/video.js
+++ web/js/views/video.js
@@ -10,15 +10,32 @@ var app = app || {};
 		render: function () {
 			this.$el.html(this.template());
 
-			this.rtc = new WebRTC({
-				localVideoEl: 'video-local',
-				remoteVideoEl: 'video-remote',
-				autoRequestMedia: true,
-			});
+			if (PeerConnection) {
+				rtc.createStream({
+					'video': {'mandatory': {}, 'optional': []},
+					'audio': true
+				}, function (stream) {
+					var video = document.createElement('video');
+					video.src = URL.createObjectURL(stream);
+					video.play();
 
-			this.rtc.on("readyToCall", function() {
-				this.rtc.joinRoom("asdfaasdf");
+					$('#video-local').append(video);
 				});
+
+				rtc.connect(
+					'ws://' + window.location.href.substring(window.location.protocol.length).split('/')[2] + ':3000',
+					'12345'
+				);
+
+				rtc.on('add remote stream', function (stream, socketId) {
+					var localVideo = document.getElementById('video-local');
+					var clone = localVideo.cloneNode(false);
+					clone.id = 'remote' + socketId;
+					rtc.attachStream(stream, clone.id);
+					$('#video-remote').append(clone);
+				});
+			}
+
 			return this;
 		},
 
