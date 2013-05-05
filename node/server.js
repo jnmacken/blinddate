be65c8a803f890e427f093aa8dce1093b9d63a5e
Andrew Davidson <aadavids@uwaterloo.ca>
2013-05-04 21:49:47 -0400

    added rct support on server with socket.io

--- node/server.js
+++ node/server.js
@@ -1,7 +1,9 @@
 var express = require('express'),
     profile = require('./routes/profiles');
  
-var app = express();
+var app = express()
+, server = require('http').createServer(app)
+, io = require('socket.io').listen(server);
  
 app.configure(function () {
     app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
@@ -14,5 +16,61 @@ app.post('/profiles', profile.addprofile);
 app.put('/profiles/:id', profile.updateprofile);
 app.delete('/profiles/:id', profile.deleteprofile);
  
+io.sockets.on('connection', function (client) {
+    // pass a message
+    client.on('message', function (details) {
+        var otherClient = io.sockets.sockets[details.to];
+
+        if (!otherClient) {
+            return;
+        }
+        delete details.to;
+        details.from = client.id;
+        otherClient.emit('message', details);
+    });
+
+    client.on('join', function (name) {
+        client.join(name);
+        io.sockets.in(name).emit('joined', {
+            room: name,
+            id: client.id
+        });
+    });
+
+    function leave() {
+        var rooms = io.sockets.manager.roomClients[client.id];
+        for (var name in rooms) {
+            if (name) {
+                io.sockets.in(name.slice(1)).emit('left', {
+                    room: name,
+                    id: client.id
+                });
+            }
+        }
+    }
+
+    client.on('disconnect', leave);
+    client.on('leave', leave);
+
+    client.on('create', function (name, cb) {
+        if (arguments.length == 2) {
+            cb = (typeof cb == 'function') ? cb : function () {};
+            name = name || uuid();
+        } else {
+            cb = name;
+            name = uuid();
+        }
+        // check if exists
+        if (io.sockets.clients(name).length) {
+            cb('taken');
+        } else {
+            client.join(name);
+            if (cb) cb(null, name);
+        }
+    });
+});
+
+
+
 app.listen(3000);
 console.log('Listening on port 3000...');
