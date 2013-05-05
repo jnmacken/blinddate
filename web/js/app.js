832f81599e52f76592d86c0e68a04e84705a2436
Owen Smith <owen@omsmith.ca>
2013-05-05 04:25:50 -0400

    start passing model

--- web/js/app.js
+++ web/js/app.js
@@ -3,6 +3,14 @@ var app = app || {};
 $(function () {
 	'use strict';
 
+	var id = $.cookie('bdid');
+	if (!id) {
+		id = Math.random() % 10000;
+		$.cookie('bdid', id);
+	}
+
 	// kick things off by creating the `App`
-	new app.AppView();
+	new app.AppView({
+		profile: profiles.fetch(id);
+	});
 });
