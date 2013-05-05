9d8126c0590c43675baeb6d1a9ca733f1c698c25
Josh Mackenzie <jnmacken@uwaterloo.ca>
2013-05-05 08:15:45 -0400

    added gender, commented out though

--- web/js/app.js
+++ web/js/app.js
@@ -4,7 +4,7 @@ $(function () {
 	'use strict';
 
 	var id = Math.random() % 10000;
-	app.profile = new app.Profile({'id' : '5185b42712739d0000000001'});
+	app.profile = new app.Profile({'id' : '518623003d2f1c9024000003'});
 	app.profile.fetch();
 	// kick things off by creating the `App`
 	new app.AppView();
